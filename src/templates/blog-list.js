8c36893b9974c947ece6f8d1618f5daab1826c9e
Bradley Ramos <bradleyramos2021@u.northwestern.edu>
2020-11-17 16:58:52 -0600

    update nnn

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -71,7 +71,7 @@ class BlogIndex extends React.Component {
     
     const { data } = this.props
     const { currentPage, numPages } = this.props.pageContext
-    const blogSlug = '/projects/' 
+    const blogSlug = '/blog/' 
     const isFirst = currentPage === 1
     const isLast = currentPage === numPages
     const prevPage = currentPage - 1 === 1 ? blogSlug : blogSlug + (currentPage - 1).toString()
@@ -95,10 +95,10 @@ class BlogIndex extends React.Component {
     return (
       <Layout className="blog-page">
         <SEO
-          title={"Projects — Page " + currentPage + " of " + numPages}
-          description={"Stackrole base projects page " + currentPage + " of " + numPages }
+          title={"Blog — Page " + currentPage + " of " + numPages}
+          description={"Stackrole base blog page " + currentPage + " of " + numPages }
         />
-        <h1>Projects</h1>
+        <h1>My Work</h1>
         <div className="grids col-1 sm-2 lg-3">
           {posts}
         </div>
